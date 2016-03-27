ba7cfbed307fcf80eb7a7f2499fb2c9711dc0c67
Ossi Hanhinen <ossi.hanhinen@gmail.com>
2016-03-27 22:36:36 +0300

    Add type annotations

--- Character.elm
+++ Character.elm
@@ -12,29 +12,36 @@ princess =
     Character "Princess Peach" "Pink"
 
 
+mario : Character
 mario =
     Character "Mario" "Red"
 
 
+luigi : Character
 luigi =
     Character "Luigi" "Green"
 
 
+boo : Character
 boo =
     Character "Boo" "White"
 
 
+bigBoo : Character
 bigBoo =
     Character "Big Boo" "Snow White"
 
 
+mechaKoopa : Character
 mechaKoopa =
     Character "MechaKoopa" "Metallic Green"
 
 
+goomba : Character
 goomba =
     Character "Goomba" "Brown"
 
 
+bill : Character
 bill =
     Character "Bullet Bill" "Black"
